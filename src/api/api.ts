c81e8077d371ba22983ccff5e759b9942b132f86
SettarovAli <settarov.ali@gmail.com>
2022-08-06 22:02:04 +0300

    Add api and axios

---
+++ src/api/api.ts
@@ -0,0 +1,13 @@
+import axios from 'axios';
+import { EnvironmentEnum } from '../variables/global';
+
+export default axios.create({
+  baseURL:
+    process.env.NODE_ENV === EnvironmentEnum.DEVELOPMENT
+      ? process.env.REACT_APP_DEVELOPMENT_API
+      : process.env.REACT_APP_PRODUCTION_API,
+  headers: {
+    'Content-Type': 'application/json',
+    Accept: 'application/json',
+  },
+});
